d600e241241fa8acc0ed91a6d4c4dd4250898670
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2023-02-04 13:00:08 -0800

    Split function stub
    Added stub for split function
    Added split angle variables
    Added root prefab reference in player controller
    Updated Root Prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0.0f, 20.0f)] private float speed = 0.0f;
     [SerializeField, Range(40.0f, 180.0f)] private float rotationSpeed = 40.0f;
     [SerializeField, Range(1.0f, 3.0f)] private float accel = 1.0f;
-    
+    [SerializeField] private GameObject rootPrefab = null;
+
+    [SerializeField, Range(10.0f, 90.0f)] private float minSplitAngle = 30.0f;
+    [SerializeField, Range(10.0f, 90.0f)] private float maxSplitAngle = 90.0f;
+
     private Vector2 direction = Vector2.down;
 
+    private void Awake()
+    {
+#if UNITY_EDITOR
+        if (rootPrefab == null)
+        {
+            Debug.LogError("Root prefab not set");
+        }
+#endif
+    }
 
     void Update()
     {
@@ -25,6 +38,13 @@ public class PlayerController : MonoBehaviour
         // Change the position based on the direction (normalized) and speed
         Vector2 move = direction * speed * Time.deltaTime;
         transform.position = transform.position + (Vector3)move;
+
+
+        // Debug for split
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Split();
+        }
     }
 
     public void SetSpeed(float newSpeed)
@@ -40,5 +60,11 @@ public class PlayerController : MonoBehaviour
     public void RotateDirection(Quaternion rot)
     {
         direction = rot * direction;
+    }
+
+    public void Split()
+    {
+
+
     }
 }
