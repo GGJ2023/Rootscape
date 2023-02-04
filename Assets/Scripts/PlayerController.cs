ad691813378589b58ef0a9b79065a4b2c70bdb12
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2023-02-04 14:32:19 -0800

    update root branching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(0.0f, 20.0f)] private float speed = 0.0f;
-    [SerializeField, Range(40.0f, 180.0f)] private float rotationSpeed = 40.0f;
-    [SerializeField, Range(1.0f, 3.0f)] private float accel = 1.0f;
-    [SerializeField] private GameObject rootPrefab = null;
+    [SerializeField, Range(0.0f, 20.0f)]
+    private float speed = 0.0f;
 
-    [SerializeField, Range(10.0f, 90.0f)] private float minSplitAngle = 30.0f;
-    [SerializeField, Range(10.0f, 90.0f)] private float maxSplitAngle = 90.0f;
+    [SerializeField, Range(40.0f, 180.0f)]
+    private float rotationSpeed = 40.0f;
+
+    [SerializeField, Range(1.0f, 3.0f)]
+    private float accel = 1.0f;
+
+    [SerializeField]
+    private GameObject rootPrefab = null;
+
+    [SerializeField, Range(10.0f, 90.0f)]
+    private float minSplitAngleLeft = 30.0f;
+
+    [SerializeField, Range(10.0f, 90.0f)]
+    private float maxSplitAngleLeft = 90.0f;
+
+    [SerializeField, Range(-10.0f, -90.0f)]
+    private float maxSplitAngleRight = -30.0f;
+
+    [SerializeField, Range(-10.0f, -90.0f)]
+    private float minSplitAngleRight = -90.0f;
 
     private Vector2 direction = Vector2.down;
+
     private bool canSplit = true;
 
     private void Awake()
@@ -30,16 +47,16 @@ public class PlayerController : MonoBehaviour
 
         // Calculate how much to rotate this frame
         float rotationAmount = horizontal * rotationSpeed * Time.deltaTime;
-        Quaternion rotation = Quaternion.Euler(Vector3.forward * -rotationAmount);
-        RotateDirection(rotation);
+        Quaternion rotation =
+            Quaternion.Euler(Vector3.forward * -rotationAmount);
+        RotateDirection (rotation);
 
         // Calculate how much to accelerate this frame
         ModifySpeed(vertical * accel * Time.deltaTime);
 
         // Change the position based on the direction (normalized) and speed
         Vector2 move = direction * speed * Time.deltaTime;
-        transform.position = transform.position + (Vector3)move;
-
+        transform.position = transform.position + (Vector3) move;
 
         // Debug for split
         if (Input.GetKeyDown(KeyCode.Space))
@@ -80,10 +97,23 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        
+
+        float rand = Random.Range(-1, 1);
+        float rotationAmount;
+
         // Get a random rotation angle between the bounds
-        float rotationAmount = Random.Range(minSplitAngle, maxSplitAngle);
-        Quaternion rotation = Quaternion.Euler(Vector3.forward * rotationAmount);
+        if (rand < 0)
+        {
+            rotationAmount = Random.Range(minSplitAngleLeft, maxSplitAngleLeft);
+        }
+        else
+        {
+            rotationAmount =
+                Random.Range(minSplitAngleRight, maxSplitAngleRight);
+        }
+        Debug.Log (rand);
+        Quaternion rotation =
+            Quaternion.Euler(Vector3.forward * rotationAmount);
 
         // Instatiate the new root
         GameObject newRoot = Instantiate(rootPrefab);
@@ -95,7 +125,7 @@ public class PlayerController : MonoBehaviour
         newPC.SetDirection(rotation * direction);
 
         // Set the speed to be equal
-        newPC.SetSpeed(speed);
+        newPC.SetSpeed (speed);
 
         // Don't allow the new root to split
         newPC.SetCanSplit(false);
