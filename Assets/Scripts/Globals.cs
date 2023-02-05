aaea52d0be7de86a37dd6bb4b93b44face906d00
Alex Hartford <alexanderhartford@gmail.com>
2023-02-05 11:22:23 -0800

    tweak

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -16,7 +16,7 @@ public class Globals : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(distanceTravelled.ToString());
-        Debug.Log(numberOfSplits.ToString());
+        Debug.Log(distanceTravelled.ToString());
+        //Debug.Log(numberOfSplits.ToString());
     }
 }
