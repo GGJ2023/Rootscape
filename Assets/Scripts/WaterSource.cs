6559927092faa920c396103657b31c514eb62a31
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2023-02-04 17:05:38 -0800

    Water source shrink

--- Assets/Scripts/WaterSource.cs
+++ Assets/Scripts/WaterSource.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class WaterSource : MonoBehaviour
 {
+    [SerializeField]
+    private float deathTimer = 2.0f;
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController pc;
@@ -12,6 +15,29 @@ public class WaterSource : MonoBehaviour
         if (isRoot)
         {
             pc.ResetLifetime();
+            pc.Split();
+            StartCoroutine(Shrivel());
         }
+    }
+
+    private IEnumerator Shrivel()
+    {
+        // Disable the collider
+        Collider2D col = GetComponent<Collider2D>();
+        col.enabled = false;
+
+        Vector3 vel = Vector3.zero;
+
+        // Shrink the water over time
+        while (deathTimer > 0.0f)
+        {
+            deathTimer -= Time.deltaTime;
+            Vector3 scale = transform.localScale;
+            scale = Vector3.SmoothDamp(scale, Vector3.zero, ref vel, deathTimer);
+            transform.localScale = scale;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
