4b5067ab9658c2fed64609db1d323faa084d9839
Alex Hartford <alexanderhartford@gmail.com>
2023-02-05 15:16:53 -0800

    tree accumulator

--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class TreeSpawner : MonoBehaviour
 {
+    public bool GameSpawner = true;
+    public TreeSpawner LevelSpawner;
     public GameObject[] trees;
+    public GameObject instance;
+    public int next = 0;
+    public bool loadNext = true;
     // Start is called before the first frame update
     void Start()
     {
-        int choice = Random.Range(0, trees.Length); 
-        GameObject newOne = Instantiate(trees[choice], transform.position, transform.rotation);
-        tree newTree = newOne.GetComponent<tree>();
-        newTree.curveAmount = Random.Range(0.0f, 0.0001f); 
-        newTree.splitThresholdOne = Random.Range(1.0f, 2.0f); 
-        newTree.splitThresholdTwo = Random.Range(2.0f, 4.0f); 
-        newTree.splitThresholdThree = Random.Range(3.0f, 5.0f); 
-        newTree.splitThresholdFour = Random.Range(4.0f, 6.0f); 
+        if(GameSpawner)
+        {
+            int choice = Random.Range(0, trees.Length); 
+            instance = Instantiate(trees[choice], transform.position, transform.rotation);
+            tree newTree = instance.GetComponent<tree>();
+            newTree.curveAmount = Random.Range(0.0f, 0.0001f); 
+            newTree.splitThresholdOne = Random.Range(1.0f, 2.0f); 
+            newTree.splitThresholdTwo = Random.Range(2.0f, 4.0f); 
+            newTree.splitThresholdThree = Random.Range(3.0f, 5.0f); 
+            newTree.splitThresholdFour = Random.Range(4.0f, 6.0f); 
+        }
+        else
+        {
+            foreach (GameObject tree in trees)
+            {
+                if(tree)
+                {
+                    GameObject newOne = Instantiate(tree, transform.position, transform.rotation);
+                    tree newTree = newOne.GetComponent<tree>();
+                    newTree.height = 0; 
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(GameSpawner)
+        {
+            if(GameObject.FindGameObjectsWithTag("Root").Length <= 0 && loadNext)
+            {
+                LevelSpawner.trees[LevelSpawner.next % LevelSpawner.trees.Length] = instance;
+                ++LevelSpawner.next;
+                loadNext = false;
+            }
+        }
     }
 }
+
+
