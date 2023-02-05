6a7ebdec6bd80d797618cf70027ab25a0b379045
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2023-02-05 14:37:04 -0800

    adding SFXs

--- Assets/Scripts/Enemy/Rock.cs
+++ Assets/Scripts/Enemy/Rock.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof (BoxCollider2D))]
 public class Rock : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController pc;
-        bool isRoot = collision.gameObject.TryGetComponent<PlayerController>(out pc);
+        bool isRoot =
+            collision.gameObject.TryGetComponent<PlayerController>(out pc);
         if (isRoot)
         {
+            FindObjectOfType<SFXManager>().Play("rockSFX");
             pc.KillRoot();
         }
     }
